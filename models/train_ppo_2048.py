913a40fa1313706e333388a6eca3accb048f5091
tejpshah <55666467+tejpshah@users.noreply.github.com>
2023-04-10 14:17:51 -0400

    next hyperparameter setting run

--- models/train_ppo_2048.py
+++ models/train_ppo_2048.py
@@ -38,18 +38,18 @@ if __name__ == "__main__":
 
     # Hyperparameters for model
     SHARED_HIDDEN_LAYER_SIZE= 256
-    NUM_SHARED_LAYERS = 1
+    NUM_SHARED_LAYERS = 2
     ACTIVATION = nn.ReLU()
-    PPO_CLIP_VAL = 0.10
-    PPO_POLICY_LR = 1e-5
-    PPO_VALUE_LR = 5e-5
-    PPO_EPOCHS = 60
-    VAL_EPOCHS = 60
+    PPO_CLIP_VAL = 0.20
+    PPO_POLICY_LR = 3e-5
+    PPO_VALUE_LR = 3e-4
+    PPO_EPOCHS = 40
+    VAL_EPOCHS = 40
     KL_TARGET = 0.02
     N_EPISODES = 15000
     PRINT_FREQ = 1
-    NUM_ROLLOUTS = 10
-    SAVE_FREQ = 500 
+    NUM_ROLLOUTS = 8
+    SAVE_FREQ = 200 
 
     ###  TRAINS MODEL USING PROXIMAL POLICY OPTIMIZATION FOR 2048 ###
 
